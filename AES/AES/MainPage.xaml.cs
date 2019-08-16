5b2158b43ad946ef57573aa854203fb4d2bed462
Yang Wang <yang.wang@oit.edu>
2019-08-15 22:39:17 -0700

    Added initial layout w inputs

--- AES/AES/MainPage.xaml.cs
+++ AES/AES/MainPage.xaml.cs
@@ -22,9 +22,131 @@ namespace AES
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private int currStep = 0;
+
+        private int[,] currPlainNumbers = new int[4, 4];
+        private int[,] currKeyNumbers = new int[4, 4];
+        private string plainBinNums;
+        private string keyBinNums;
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private void EncodeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var currTextPos = 0;
+
+            switch (currStep)
+            {
+                case 0:
+                    plainBinNums = Hex2Binary(plainTextInput.Text);
+                    keyBinNums = Hex2Binary(keyTextInput.Text);
+                    PopulateFourByFourPlain(currTextPos);
+                    PopulateFourByFourKey(currTextPos);
+                    AlignPlainLetters();
+                    AlignKeyLetters();
+                    //currStep++;
+                    encodeBtn.Content = "";
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        private string Binary2Hex(string binary)
+        {
+            string hexVal = "";
+            hexVal = Convert.ToString(Convert.ToInt32(binary, 16), 2);
+            return hexVal;
+        }
+        private string Hex2Binary(string hexvalue)
+        {
+            var binaryval = Convert.ToString(Convert.ToInt32(hexvalue, 16), 2);
+            if (binaryval.Length < 16)
+            {
+                binaryval = binaryval.PadLeft(16, '0');
+                binaryval = binaryval.ToString();
+            }
+            return binaryval;
+        }
+
+        private void PopulateFourByFourPlain(int currTextPos)
+        {
+            for (var col = 0; col < 4; col++)
+            {
+                for (var row = 0; row < 4; row++)
+                {
+                    currPlainNumbers[row, col] = plainBinNums[currTextPos] - 48;
+                    currTextPos++;
+                }
+            }
+        }
+
+        private void PopulateFourByFourKey(int currTextPos)
+        {
+            for (var col = 0; col < 4; col++)
+            {
+                for (var row = 0; row < 4; row++)
+                {
+                    currKeyNumbers[row, col] = keyBinNums[currTextPos] - 48;
+                    currTextPos++;
+                }
+            }
+        }
+        private void AlignPlainLetters()
+        {
+            // 1st column
+            OneByOne.Text = Convert.ToString(currPlainNumbers[0, 0]);
+            TwoByOne.Text = Convert.ToString(currPlainNumbers[1, 0]);
+            ThreeByOne.Text = Convert.ToString(currPlainNumbers[2, 0]);
+            FourByOne.Text = Convert.ToString(currPlainNumbers[3, 0]);
+            // 2nd column
+            OneByTwo.Text = Convert.ToString(currPlainNumbers[0, 1]);
+            TwoByTwo.Text = Convert.ToString(currPlainNumbers[1, 1]);
+            ThreeByTwo.Text = Convert.ToString(currPlainNumbers[2, 1]);
+            FourByTwo.Text = Convert.ToString(currPlainNumbers[3, 1]);
+            // 3rd column
+            OneByThree.Text = Convert.ToString(currPlainNumbers[0, 2]);
+            TwoByThree.Text = Convert.ToString(currPlainNumbers[1, 2]);
+            ThreeByThree.Text = Convert.ToString(currPlainNumbers[2, 2]);
+            FourByThree.Text = Convert.ToString(currPlainNumbers[3, 2]);
+            // 4th column
+            OneByFour.Text = Convert.ToString(currPlainNumbers[0, 3]);
+            TwoByFour.Text = Convert.ToString(currPlainNumbers[1, 3]);
+            ThreeByFour.Text = Convert.ToString(currPlainNumbers[2, 3]);
+            FourByFour.Text = Convert.ToString(currPlainNumbers[3, 3]);
+        }
+
+        private void AlignKeyLetters()
+        {
+            // 1st column
+            OneByOneK.Text = Convert.ToString(currKeyNumbers[0, 0]);
+            TwoByOneK.Text = Convert.ToString(currKeyNumbers[1, 0]);
+            ThreeByOneK.Text = Convert.ToString(currKeyNumbers[2, 0]);
+            FourByOneK.Text = Convert.ToString(currKeyNumbers[3, 0]);
+            // 2nd column
+            OneByTwoK.Text = Convert.ToString(currKeyNumbers[0, 1]);
+            TwoByTwoK.Text = Convert.ToString(currKeyNumbers[1, 1]);
+            ThreeByTwoK.Text = Convert.ToString(currKeyNumbers[2, 1]);
+            FourByTwoK.Text = Convert.ToString(currKeyNumbers[3, 1]);
+            // 3rd column
+            OneByThreeK.Text = Convert.ToString(currKeyNumbers[0, 2]);
+            TwoByThreeK.Text = Convert.ToString(currKeyNumbers[1, 2]);
+            ThreeByThreeK.Text = Convert.ToString(currKeyNumbers[2, 2]);
+            FourByThreeK.Text = Convert.ToString(currKeyNumbers[3, 2]);
+            // 4th column
+            OneByFourK.Text = Convert.ToString(currKeyNumbers[0, 3]);
+            TwoByFourK.Text = Convert.ToString(currKeyNumbers[1, 3]);
+            ThreeByFourK.Text = Convert.ToString(currKeyNumbers[2, 3]);
+            FourByFourK.Text = Convert.ToString(currKeyNumbers[3, 3]);
         }
     }
 }
